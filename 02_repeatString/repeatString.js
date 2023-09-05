2a15427c2bb946e16adc5c090dce49ca897c5ab6
Tyler <tpinzzz@users.noreply.github.com>
2023-09-05 12:14:47 -0400

    Wrote repeatString function Pass all test cases in repeatString.spec.js file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+   if (num < 0) {
+    return "ERROR"
+   }
+   x = '';
+   for (let i = 0; i < num; i++) {
+      x = x + str;
+   }    
+   return x;
 };
 
 // Do not edit below this line
