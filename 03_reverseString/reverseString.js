c55eaabf15f30e74fc2e7840c7818667750002ce
Tyler <tpinzzz@users.noreply.github.com>
2023-09-07 01:20:00 -0400

    Write sudo code for reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+   //declare variable that will be the reverse of the input string 
+   
+   //loop through each character of the input string string from 
+   //back to front and concatinate to the output variable
 
+   //return the str
+   
+   
 };
 
 // Do not edit below this line
